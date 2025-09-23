4f9bd9121abc361a2d098373c423ffdce55b218a
Luna <lunsykes06@gmail.com>
2025-09-22 20:30:41 -0400

    Update IT_106_Group_1_Project.java : adding comments

--- IT_106_Group_1_Project.java
+++ IT_106_Group_1_Project.java
@@ -1,5 +1,7 @@
 public class LabProgram {
    public static void main(String[] args) {
+    // Please delete this hello world message, this is just the base form for us
+    // Also change the LabProgram name, that's just the file that I pulled this from
     System.out.println("Hello world");
    }
 }  
